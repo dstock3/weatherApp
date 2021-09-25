5bbdf009c4a330db9dedf8bf99b43ae84cbf2bc0
dstock3 <dstock3@gmail.com>
2021-09-25 12:12:11 -0500

    Created a search bar and a weather function that requests data from the open weather API.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+function elementBuilder (elType, className, parent) {
+  const newElement = document.createElement(elType);
+  newElement.classList.add(className);
+  parent.appendChild(newElement);
+  return newElement;
+};
+
+const body = document.querySelector("body");
+
+const weather = async (term) => {
+  //const weatherInfo = elementBuilder("p", "weather-info", body);
+
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${term}&appid=646bad4630202074bd6e0e37126b3203`, {mode: 'cors'});
+
+  const weatherData = await response.json();
+  console.log(weatherData)
   
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  
-  return element;
-}
-  
-document.body.appendChild(component());
+  //weatherInfo.textContent = weatherData.data.images.original.url;
+};
+
+const searchBar = elementBuilder("input", "search", body);
+searchBar.setAttribute("type", "text");
+searchBar.setAttribute("placeholder", "Search...");
+
+const button = elementBuilder("button", "img-gen", body)
+button.textContent = "Search";
+
+const searchWeather = () => {
+    let term = searchBar.value;
+    weather(term);
+};
+
+button.addEventListener("click", searchWeather);
