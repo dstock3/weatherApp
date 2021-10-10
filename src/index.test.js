bd06b58819f4bc420573c10395c18fc99b2041de
David Stockdale <dstock3@gmail.com>
2021-10-10 12:35:39 -0500

    Moved babel config file to root.

--- src/index.test.js
+++ src/index.test.js
@@ -1,2 +1,7 @@
 import { process } from './index'
+import { mockData } from './mockData'
 
+test('process function returns a forecast array with the appropriate length', () => {
+    let processedData = process(mockData)
+    expect(processedData.forecastArray.length).toBe(8);
+});
