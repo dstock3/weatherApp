3f8ca1bae5977aff777083934d7d302537a957e0
David Stockdale <dstock3@gmail.com>
2021-10-10 13:31:03 -0500

    Added test to check forecast temps.

--- src/index.test.js
+++ src/index.test.js
@@ -33,8 +33,25 @@ test ('alert array is undefined in the case of no weather alerts', () => {
     expect(alerts).toBe(undefined);
 });
 
-test("forecast array objects have the appropriate properties", () => {
+test("forecast array objects have valid properties", () => {
     for (let i = 0; i < forecast.length; i++) {
         expect(forecast[i].date).not.toBeNull();
+        expect(typeof forecast[i].temp).toBe('number');
+        expect(typeof forecast[i].high).toBe('number');
+        expect(typeof forecast[i].low).toBe('number');
+        expect(forecast[i].info).not.toBeNull();
+    };
+});
+
+test("forecast array temp values are appropriately assigned", () => {
+    for (let i = 0; i < forecast.length; i++) {
+        let temp = forecast[i].temp;
+        let high = forecast[i].high;
+        let low = forecast[i].low;
+
+        expect(high).toBeGreaterThan(low);
+        expect(temp).toBeGreaterThan(low);
+        expect(temp).toBeGreaterThan(low);
+        expect(temp).toBeLessThan(high);
     };
 });
